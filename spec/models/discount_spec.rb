055cbe948c8d6c79b3ea4db81ccb537b4bf23d17
Elom Amouzou <elom1990@yahoo.com>
2020-02-29 14:37:18 -0700

    add model test for discount

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Discount, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :percentage }
+    it { should validate_uniqueness_of :name }
+    it { should validate_uniqueness_of :percentage }
+    it { should validate_numericality_of(:percentage).is_less_than(100)}
+    it { should validate_numericality_of(:percentage).is_greater_than(0)}
+  end
+
+  describe "relationships" do
+    it {should belong_to :merchant}
+    it {should have_many(:orders)}
+  end
+end
