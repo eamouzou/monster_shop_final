9c8508ebacb4f2dcb38e9f44d932679e4347562a
Elom Amouzou <elom1990@yahoo.com>
2020-02-29 14:42:00 -0700

    edit discount model test

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Discount, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :percentage }
     it { should validate_uniqueness_of :name }
-    it { should validate_uniqueness_of :percentage }
     it { should validate_numericality_of(:percentage).is_less_than(100)}
     it { should validate_numericality_of(:percentage).is_greater_than(0)}
   end
