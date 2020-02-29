4b394e54299fe1a99f7a249aef967d5b90cb6bd2
Elom Amouzou <elom1990@yahoo.com>
2020-02-29 15:40:36 -0700

    add threshold validation

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,6 +2,8 @@ class Discount < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates_presence_of :percentage
   validates_numericality_of :percentage, greater_than: 0, less_than: 100
+  validates_presence_of :threshold
+  validates_numericality_of :threshold, greater_than: 4, less_than: 61
 
   belongs_to :merchant
   has_many :orders
