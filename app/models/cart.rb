3c11722d149e8e376d1fe87f096dadc5f53581af
Elom Amouzou <elom1990@yahoo.com>
2020-03-01 15:19:33 -0700

    revise cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -31,4 +31,32 @@ class Cart
       Item.find(item_id).price * quantity
     end
   end
+
+  def discount_subtotal(item, quantity)
+    discounts = Discount.all.find_all do |discount|
+      discount.merchant == item.merchant && quantity >= discount.threshold
+    end
+
+    discount = discounts.max_by {|discount| discount.threshold}
+    ((item.price * (100 - discount.percentage) / 100) * quantity).to_f.round(2)
+  end
+
+  def discount_total_eligible?
+    @contents.any? do |item_id, quantity|
+      Discount.all.any? do |discount|
+        discount.merchant == Item.find(item_id).merchant
+      end
+    end
+  end
+
+  def discount_total
+    x = @contents.sum do |item_id, quantity|
+      item = Item.find(item_id)
+      if item.discount_apply?(quantity)
+        discount_subtotal(item, quantity)
+      else
+        item.price * quantity
+      end
+    end
+  end
 end
