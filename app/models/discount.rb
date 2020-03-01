4f991ea6aee448d09d15fde9ef6c95211d0fe7f8
Elom Amouzou <elom1990@yahoo.com>
2020-03-01 15:09:38 -0700

    add relationships for one to many between discount and item; tests pass

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -6,6 +6,7 @@ class Discount < ApplicationRecord
   validates_numericality_of :threshold, greater_than: 4, less_than: 61
 
   belongs_to :merchant
+  has_many :items
   has_many :order_discounts
   has_many :orders, through: :order_discounts
 
