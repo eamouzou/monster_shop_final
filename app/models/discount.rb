96006ad6b13b5d1905edce940a44cc45ed50fdc5
Elom Amouzou <elom1990@yahoo.com>
2020-03-01 12:48:23 -0700

    revise model method

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -8,4 +8,10 @@ class Discount < ApplicationRecord
   belongs_to :merchant
   has_many :order_discounts
   has_many :orders, through: :order_discounts
+
+  def generate_order_discounts
+    merchant.item_orders.pluck(:order_id).each do |order_id|
+      OrderDiscount.create(discount_id: self.id, order_id: order_id)
+    end
+  end
 end
