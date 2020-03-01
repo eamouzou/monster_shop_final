b727272e75350a8357630f7b8dfbc20f2002e1be
Elom Amouzou <elom1990@yahoo.com>
2020-02-29 21:27:36 -0700

    add create action and helper methods

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -14,4 +14,26 @@ class Merchant::DiscountsController < Merchant::BaseController
   def show
     @discount = Discount.find(params[:discount_id])
   end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = @merchant.discounts.create(discount_params)
+    create_redirect_process(merchant = @merchant, discount)
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :percentage, :threshold)
+  end
+
+  def create_redirect_process(merchant, discount)
+    redirect_to "/merchant/#{merchant.id}/discounts" if discount.save == true
+    create_save_error(merchant, discount) if discount.save == false
+  end
+
+  def create_save_error(merchant, discount)
+    flash[:error] = discount.errors.full_messages.to_sentence
+    redirect_to "/merchant/#{merchant.id}/discounts/new"
+  end
 end
