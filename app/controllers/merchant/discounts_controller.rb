55f0e90c334938bb0d8f9a10cd576531802fbbaf
Elom Amouzou <elom1990@yahoo.com>
2020-02-29 20:59:59 -0700

    add merchant discounts controller and actions

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,16 @@ class Merchant::DiscountsController < Merchant::BaseController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def edit
+    @discount = Discount.find(params[:discount_id])
+  end
+
+  def show
+    @discount = Discount.find(params[:discount_id])
+  end
 end
